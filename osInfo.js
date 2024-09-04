3e2992bf5331c169d0bd0d8e38e5d0c198f8fa50
tukkedd <jeamki2501@gmail.com>
2024-09-03 23:35:12 -0400

    promises readfiles and more

--- osInfo.js
+++ osInfo.js
@@ -1,13 +1,13 @@
-const os = require('node:os')
+import { platform, release, arch, cpus, freemem, totalmem, uptime } from 'node:os';
 
 console.log('info del sistema operaivo');
 console.log('---------------------------------');
 
-console.log('nombre del sistema operativo', os.platform());
-console.log('Version del sistema ', os.release());
-console.log('arquitectura', os.arch());
-console.log('cpus', os.cpus());
-console.log('Memoria libre ', os.freemem() / 1024 / 1024);
-console.log('Memoria total ', os.totalmem() / 1024 / 1024);
-console.log('uptime ', os.uptime() / 60 / 60);
+console.log('nombre del sistema operativo', platform());
+console.log('Version del sistema ', release());
+console.log('arquitectura', arch());
+console.log('cpus', cpus());
+console.log('Memoria libre ', freemem() / 1024 / 1024);
+console.log('Memoria total ', totalmem() / 1024 / 1024);
+console.log('uptime ', uptime() / 60 / 60);
 
