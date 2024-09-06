73c7ba41d577d447d40942be3676451017db6dba
tukkedd <jeamki2501@gmail.com>
2024-09-06 19:48:25 -0400

    process.js

---
+++ process.js
@@ -0,0 +1,13 @@
+console.log();
+
+//podemos controlar eventos del proceso
+// process.on('exit', () => {
+//     limpiar los recursos
+// })
+
+
+// current working directory (cwd)
+console.log(process.cwd());
+
+// Platform
+console.log(process.env.PEPITO);
